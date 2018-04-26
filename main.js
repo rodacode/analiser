f93894a6cb1fa257133139535f0dfc8172094af0
volatildub <volatilinfo@yahoo.es>
2018-04-26 16:00:51 +0200

    added input url, playing with async

--- main.js
+++ main.js
@@ -1,25 +1,50 @@
+var form;
+var str;
+var inputUrl;
 
-function analyzer(form){
-	var str;
-	var y = $(str).html();
-	var urlInput = form.inputbox.value;
-	console.log(urlInput);
-	$.getJSON('http://www.whateverorigin.org/get?url=http://' + encodeURIComponent(form.inputbox.value) + '&callback=?', function(data){
-		str =data.contents;
-		console.log(str);
+
+var y = $(str).html();
+
+
+async function getUrl(form){
+	console.log('getUrl: '+form.inputbox.value);
+	inputUrl = form.inputbox.value;
+	console.log(inputUrl);	
+	var x = $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent('http://'+ inputUrl) + '&callback=?', function(data){
+		return str = data.contents;
+		console.log('CODE:::::'+str);
 	});
- 	var regexTitle = /<title>/;
+	console.log('x: '+str);
+    await analyzer(str);
 
- 	var code = document.createTextNode(str); 
- 	var showCode = document.getElementById("showCode");
- 	showCode.appendChild(code); 
- 	if(regexTitle.test(str)){
- 		var title ="title Tag";
-	  	document.getElementById("show").innerHTML = "<p class='analisis-line'>" + title + "</p>"; 
+}
+
+
+
+function analyzer(str){
+	showSourceCode(str);
+	checkTags(str);
+	console.log('analyzer: ' + str);
+}
+
+function showSourceCode(str){
+
+	var y = $(str).html();
+	var code = document.createTextNode(str); 
+	var showCode = document.getElementById("showCode");
+	showCode.appendChild(code); 
+	console.log('showSourceCode: ' + str);
+}
+
+function checkTags(str){
+	console.log('checkTags' + str);
+	var regexTitle = /<title>/;
+	if(regexTitle.test(str)){
+		var title ="title Tag";
+		document.getElementById("show").innerHTML = "<p class='analisis-line'>" + title + "</p>"; 
 	} 
 	else{
- 		answer="We couldn´t find any title tag";
- 	 	document.getElementById("show").innerHTML = answer; 
- 	}
- }
-
+		answer="We couldn´t find any title tag";
+		document.getElementById("show").innerHTML = answer; 
+	}
+}
