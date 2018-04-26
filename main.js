cd9ed9f6e016a692895b01113543d335ab4611ff
volatildub <volatilinfo@yahoo.es>
2018-04-26 11:44:06 +0200

    css added working with callbacks

--- main.js
+++ main.js
@@ -1,31 +1,25 @@
-var str;
-var x = $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent('http://google.com') + '&callback=?', function(data){
-	str =data.contents;
-	console.log(str);
-});
-
-var y = $(str).html();
 
 function analyzer(form){
- var regex = /<title>/;
- //var str = form.inputbox.value;
- if(regex.test(str)){
- 	answer="We found a title Tag";
-	  document.getElementById("show").innerHTML = "<p class='analisis-line'>" +answer + "</p>"; 
-	  var code = document.createTextNode(str); 
-	  var showCode = document.getElementById("showCode");
-	  showCode.appendChild(code); 
-	 // var codeText =  code.replace("<!doctype html>", "aaa");
-	//document.getElementById("showCode").innerHTML = "<!--" + code + "-->";
-	console.log(code);
- 	} else{
- 	answer="We couldn´t find any title tag";
- 	 document.getElementById("show").innerHTML = answer; 
+	var str;
+	var y = $(str).html();
+	var urlInput = form.inputbox.value;
+	console.log(urlInput);
+	$.getJSON('http://www.whateverorigin.org/get?url=http://' + encodeURIComponent(form.inputbox.value) + '&callback=?', function(data){
+		str =data.contents;
+		console.log(str);
+	});
+ 	var regexTitle = /<title>/;
 
+ 	var code = document.createTextNode(str); 
+ 	var showCode = document.getElementById("showCode");
+ 	showCode.appendChild(code); 
+ 	if(regexTitle.test(str)){
+ 		var title ="title Tag";
+	  	document.getElementById("show").innerHTML = "<p class='analisis-line'>" + title + "</p>"; 
+	} 
+	else{
+ 		answer="We couldn´t find any title tag";
+ 	 	document.getElementById("show").innerHTML = answer; 
  	}
  }
 
-
- var h = document.createElement("H1")                // Create a <h1> element
-var t = document.createTextNode("Hello World");     // Create a text node
-h.appendChild(t); 
