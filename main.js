9692f921f4e35d1dbb416281279a41295475168c
volatildub <volatilinfo@yahoo.es>
2018-05-09 16:33:41 +0200

    colors added

--- main.js
+++ main.js
@@ -13,36 +13,11 @@ function getUrl(form){
 	console.log(inputUrl);	
 	$.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent('http://'+ inputUrl) + '&callback=?', function(data){
 		showSourceCode(data.contents);
+		checkTags(data.contents);
 		console.log('CODE:'+data.contents);
 		str = data.contents;
 	});
 
-};
-
-
-
-/*
-
-// url (required), options (optional)
-fetch('http://www.whateverorigin.org/get?url=' + encodeURIComponent('http://'+ inputUrl), {
-	method: 'get',
-	headers: myHeaders
-}).then(function(response) {
-	console.log(response);
-}).catch(function(err) {
-    // Error :(
-});
-
-
-
-*/
-
-function analyzer(str){
-
-	checkTags(str);
-	console.log('analyzer: ' +inputUrl);
-
-	console.log('analyzer: ' + str);
 };
 
 function showSourceCode(str){
@@ -57,12 +32,21 @@ function showSourceCode(str){
 function checkTags(str){
 	console.log('checkTags' + str);
 	var regexTitle = /<title>/;
+	var regexH1 = /<h1>/;
 	if(regexTitle.test(str)){
 		var title ="title Tag";
-		document.getElementById("show").innerHTML = "<p class='analisis-line'>" + title + "</p>"; 
+		document.getElementById("show").innerHTML = "<p class='analisis-line text-center'>" + title + "</p>"; 
 	} 
 	else{
 		answer="We couldn´t find any title tag";
-		document.getElementById("show").innerHTML = answer; 
+		document.getElementById("show").append(answer);
+	}
+	if(regexH1.test(str)){
+		var h1 ="h1 Tag";
+		document.getElementById("show").append("<p class='analisis-line text-center'>" + h1 + "</p>"); 
+	} 
+	else{
+		answer="We couldn´t find any h1 tag";
+		document.getElementById("show").append(answer); 
 	}
 };
